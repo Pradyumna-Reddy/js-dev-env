de6d42ab0da363e6ae2e5247036fc994ed6e49e2
Pradyumna <pradyum2411@gmail.com>
2021-06-14 22:34:23 +0530

    Web Server and Local Tunnel

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+
+var port = 4343;
+var app = express();
+
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(error){
+    if(error){
+        console.log(error);
+    }else{
+       open('http://localhost:'+port);
+    }
+})
